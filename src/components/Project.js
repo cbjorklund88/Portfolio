501c085fea7cc4ca83a95fe70e76078d9178399a
cbjorklund88 <clara.bjorklund@hotmail.com>
2018-10-10 19:16:48 +0200

    added info on Project page

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 import productsJson from "../products.json"
+import Button from "./Button/button"
 
 class Project extends React.Component {
 
@@ -17,13 +18,33 @@ class Project extends React.Component {
     })
 
     return (
-      <div className="case-container">
-      <Link to="/LandingPage">&#8656; Back</Link>
+      <div className="project-container">
+        <Link to="/LandingPage">&#8656; Back</Link>
         <div className="heroImage">
           <img src={project.imageHero} alt="my Work" />
         </div>
-        <h1>{project.title}</h1>
+        <div className="project-text-container">
+          <h1>{project.title}</h1>
+          <p>{project.description}</p>
+          <Button>See it live</Button>
         </div>
+        <div className="skills-list-projects-container">
+
+            <div className="skillsListCode">
+              <h5>Code</h5>
+              <p>{project.code}</p>
+            </div>
+            <div className="skillsListToolbox">
+              <h5>Toolbox</h5>
+              <p>{project.Toolbox}</p>
+            </div>
+            <div className="skillsListMore">
+              <h5>More</h5>
+              <p>{project.More}</p>
+            </div>
+        </div>
+        <Button>See it live</Button>
+      </div>
     )
   }
 
