5991a9964e8688e8b234f78a54791bdc7ac1f437
cbjorklund88 <clara.bjorklund@hotmail.com>
2018-10-26 06:22:11 +0200

    Updates

--- precache-manifest.d9bba0c28f855e5da91a735f5a10fb34.js
+++ precache-manifest.d9bba0c28f855e5da91a735f5a10fb34.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/Portfolio/static/js/runtime~main.3b961160.js"
   },
   {
-    "revision": "d610f834f815dc249cce",
-    "url": "/Portfolio/static/js/main.d610f834.chunk.js"
+    "revision": "0131aaa3318b6ecf15e6",
+    "url": "/Portfolio/static/js/main.0131aaa3.chunk.js"
   },
   {
     "revision": "e2579ee3a9f753386e97",
     "url": "/Portfolio/static/js/1.e2579ee3.chunk.js"
   },
   {
-    "revision": "d610f834f815dc249cce",
+    "revision": "0131aaa3318b6ecf15e6",
     "url": "/Portfolio/static/css/main.a3d0d71f.chunk.css"
   },
   {
-    "revision": "f231a222a227f473704bb4d4f168de29",
+    "revision": "97e24ca9082174f147d0eebed6ec995c",
     "url": "/Portfolio/index.html"
   }
 ];
